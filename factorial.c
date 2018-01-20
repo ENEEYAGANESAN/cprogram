39a7a933a087685f13c400ca5c1f61c2b215c4a3
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-01-20 19:57:03 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,i,factorial=1;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+factorial=factorial*1;
+printf("it is factorial number");
+return 0;
+}
