a80931367189c104cb1e5b0c1fd962da30cb5052
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-01-24 18:57:56 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int char[i];
+string str="good girl";
+printf("enter the character: ");
+scanf("%d",&char[i]);
+str=strlen(good girl);
+scanf("%s",&str);
+return 0;
+}
