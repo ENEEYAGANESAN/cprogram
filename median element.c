89201e0b03d3956ac9be774899333796a00b3596
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-01-24 07:33:12 -0300

    Create median element.c

---
+++ median element.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+inta[25],m,i;
+float mean=0,sum=0;
+printf("enter the terms");
+scanf("%d",&m);
+for(i=1;i<=m;i++)
+{
+scanf("%d",a[i]));
+}
+sum=sum+a[i];
+avg=sum/m;
+printf("mean entered number is %f",mean);
+}
