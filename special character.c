eab6a13e558a76f6c125d52d51e6ede2c5622ad2
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-01-31 16:05:53 +0530

    Create special character.c

---
+++ special character.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int count =0,special character;
+    printf("enter the string");
+    get(str);
+    for(count=0;str[count]!=null;count++)
+    if(str[count]>='!' && str[count]<='+")
+    {
+    printf("the special character is:%d",special character);
+    }
+    else
+    {
+    printf("there is no special characters");
+    } 
+    return 0;
+    }
+    
