1831a7717257d8f22543db8321d115087f9be05b
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-02-05 16:27:08 +0530

    Create pro 44.c

---
+++ pro 44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("enter the number");
+scanf("%d",&i);
+if(i>=1) && (i<=10)
+{
+printf(" it is in range");
+}
+else
+{
+printf("it is not in range");
+}
+return 0;
+}
