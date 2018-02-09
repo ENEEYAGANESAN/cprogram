38758647ccae9ca73f5a0828b007e7dfd4ef2ef9
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-02-09 16:01:46 +0530

    Create pro 50.c

---
+++ pro 50.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,n,b;
+printf("enter the value a");
+scanf("%d",&a);
+printf("enter the power value n");
+scanf("%d",&n);
+b=a^n;
+printf("the result is :%d",b);
+return 0;
+}
