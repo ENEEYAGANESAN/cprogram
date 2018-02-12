68ec30c87aeac71ec3900880503d1ab58ef0ab9e
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-02-12 16:41:21 +0530

    Create pro 55.c

---
+++ pro 55.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,m,b;
+    printf("enter the value");
+    scanf("%d%d",&n,&m);
+    b=n*m;
+    if(b%2==0)
+{
+    printf("it is even number");
+}
+else
+{
+    printf("it is odd number");
+}
+return 0;
+}
+
