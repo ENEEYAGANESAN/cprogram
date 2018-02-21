966d0f5776d2e92207f15579fcc3f7c65d94fc10
ENEEYAGANESAN <35128023+ENEEYAGANESAN@users.noreply.github.com>
2018-02-21 16:41:30 +0530

    Create pro 52.c

---
+++ pro 52.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+switch(a)
+{
+case 0:
+printf("one");
+case 1:
+printf("two");
+case 2:
+printf("three");
+}
+return 0;
+}
